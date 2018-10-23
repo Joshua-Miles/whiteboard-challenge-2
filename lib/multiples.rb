f87fe67d9ba726e4d1778ea3162b038b9e534afc
joshua-miles <joshua@christetulit.org>
2018-10-23 10:46:02 -0500

    solution

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    multiples = []
+    limit.times do | number | 
+        if number != 0 && (number % 3 === 0 || number % 5 === 0)
+            multiples << number
+        end
+    end
+    multiples
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).reduce(&:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+    attr_reader :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+
+    def collect_multiples
+        multiples = []
+        limit.times do | number | 
+            if number != 0 && (number % 3 === 0 || number % 5 === 0)
+                multiples << number
+            end
+        end
+        multiples
+    end
+
+    def sum_multiples
+        collect_multiples.reduce(&:+)
+    end
+end
